151fd0d1d932cfe29b854a6f71725d7ba24977d7
Claire Volinski <cdv230@lehigh.edu>
2026-09-09 15:20:52 -0400

    Add files via upload

---
+++ Programming-Activities/Program3.py
@@ -0,0 +1,4 @@
+# This program was created in Arduino Lab for MicroPython
+# variable to store my name
+name = "Claire Volinski"
+print(name) # output the contents of the variable "name" into the REPL
