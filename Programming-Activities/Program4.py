944055525d77035ebf7800ca1fb070699ee0ba94
Claire Volinski <cdv230@lehigh.edu>
2026-09-09 15:52:36 -0400

    Add files via upload

---
+++ Programming-Activities/Program4.py
@@ -0,0 +1,17 @@
+# Blinking program
+
+# incorporate/include modules
+import machine #module with all the microcontroller stuff
+import time #module with all time methods
+
+# Make the led object
+# Green led is GPIO Pin 0
+led = machine.Pin(0, machine.Pin.OUT)
+
+# Infinite loop
+while True:
+  led.value(1) # turn on the LED
+  time.sleep(0.25) # 0.25 second delay
+  led.value(0) # turn off the LED
+  time.sleep(0.25) # delay again
+
